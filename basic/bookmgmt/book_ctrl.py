40af988f5561dced0c845614f6715a195aacd89d
leeyunjai82 <leeyunjai@circul.us>
2021-07-29 20:04:20 +0900

    update

--- basic/bookmgmt/book_ctrl.py
+++ basic/bookmgmt/book_ctrl.py
@@ -1,6 +1,22 @@
 import csv
 import os
 
+class Book_Info:
+  def __init__(self, item):
+    self.name = item[0]
+    self.price = item[1]
+
+  def __str__(self):
+    return "Name: {} / Price: {}".format(self.name, self.price)    
+
+  def set(self, item):
+    self.name = item[0]
+    self.price = item[1]
+  
+  def get(self):
+    return [self.name, self.price]
+
+
 class Book_Mgmt:
   def __init__(self, filename="test.csv"):
     self.filename = filename
@@ -9,16 +25,16 @@ class Book_Mgmt:
     if os.path.isfile(self.filename):
       with open(self.filename, 'r', encoding='utf-8') as f:
         for item in csv.reader(f):
-          self.items.append(item) 
+          self.items.append(Book_Info(item)) 
     else:
       with open(self.filename, 'w', newline='') as f:
         wr = csv.writer(f)
-        wr.writerows(self.items)
+        wr.writerows([item.get() for item in self.items])
  
   def save(self):
     with open(self.filename, 'w', newline='') as f:
       wr = csv.writer(f)
-      wr.writerows(self.items)
+      wr.writerows([item.get() for item in self.items])
 
   def load(self, filename):
     self.filename = filename
@@ -27,29 +43,41 @@ class Book_Mgmt:
     if os.path.isfile(self.filename):
       with open(self.filename, 'r', encoding='utf-8') as f:
         for item in csv.reader(f):
-          self.items.append(item) 
+          self.items.append(Book_Info(item)) 
     else:
       with open(self.filename, 'w', newline='') as f:
         wr = csv.writer(f)
-        wr.writerows(self.items)
+        wr.writerows([item.get() for item in self.items])
 
   def add(self, name, price):
-    self.items.append([name, price])
+    for item in self.items:
+      if item.get()[0] == name:
+        item.set([name, price])
+        self.save()
+        return
+
+    self.items.append(Book_Info([name, price]))
+    self.save()
 
   def delete(self, name):
     for item in self.items:
-      if item[0] == name:
+      if item.get()[0] == name:
         self.items.remove(item)
+    self.save()
 
   def delete_all(self):
     self.items = []
+    self.save()
 
   def search(self, name):
+    result = ""
     for item in self.items:
-      if item[0] == name:
-        print(" Book name: {}     Price: {}".format(item[0], item[1]))
+      if item.get()[0] == name:
+        result += str(item) + "\n"
+    return result
 
   def display(self):
+    result = ""
     for item in self.items:
-      print(" Book name: {}     Price: {}".format(item[0], item[1]))
-
+      result += str(item) + "\n"
+    return result
--- basic/bookmgmt/main.py
+++ basic/bookmgmt/main.py
@@ -4,50 +4,50 @@ from book_ctrl import Book_Mgmt
 if __name__ == "__main__":
   BM = Book_Mgmt()
 
-  print("## Book Management\n\n")
+  print("## Book Management\n")
 
   while True:
-    _type = input("(load|save|add|delete|search|display|quit) > ")
+    sel = input("(load|save|add|delete|search|display|quit) > ")
     
-    if _type == "load":
-      _file = input("filename > ")
-      BM.load(_file)
+    if sel == "load":
+      filename = input("filename > ")
+      BM.load(filename)
 
-    elif _type == "save":
+    elif sel == "save":
       BM.save()
     
-    elif _type == "add":
-      _name = input("name > ")
-      _price = input("price > ")
-      _confirm = input("y or n > ")
+    elif sel == "add":
+      name = input("name > ")
+      price = input("price > ")
+      confirm = input(" y or n > ")
      
-      if _confirm.upper() == 'Y':
-        BM.add(_name, _price)
+      if confirm.upper() == 'Y':
+        BM.add(name, price)
  
-    elif _type == "delete":
-      _name = input("name > ")
-      _confirm = input("y or n > ")
+    elif sel == "delete":
+      name = input("name > ")
+      confirm = input(" y or n > ")
 
-      if _confirm.upper() == 'Y':
-        BM.delete(_name)
+      if confirm.upper() == 'Y':
+        BM.delete(name)
  
-    elif _type == "delete_all":
-      _confirm = input("y or n > ")
+    elif sel == "delete_all":
+      confirm = input(" y or n > ")
 
-      if _confirm.upper() == 'Y':
+      if confirm.upper() == 'Y':
         BM.delete_all()
 
-    elif _type == "search":
-      _name = input("name > ")
-      _confirm = input("y or n > ")
+    elif sel == "search":
+      name = input("name > ")
+      confirm = input(" y or n > ")
 
-      if _confirm.upper() == 'Y':
-        BM.search(_name)
+      if confirm.upper() == 'Y':
+        print(BM.search(name))
 
-    elif _type == "display":
-      BM.display()
+    elif sel == "display":
+      print(BM.display())
  
-    elif _type == "quit":
+    elif sel == "quit":
       print("Goodbye")
       break
 
