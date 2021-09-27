cd4dfe18c20b6e32b2241af9b731e1263bf2bbb8
leeyunjai82 <leeyunjai@circul.us>
2021-09-27 21:23:13 +0900

    update

--- basic/summary.py
+++ basic/summary.py
@@ -1,6 +1,6 @@
 print('## Python Summary')
 
-""" CH1 변수와 문자열(variables and strings) """
+""" CH1 변수, 문자열, 사용자 입력(variables, strings, User input)  """
 #''' CH1 - START
 print('\n\nChapter 1')
 
@@ -16,6 +16,14 @@ print(total_message)
 
 lists = ['aaa', 'bbb', 'ccc']
 print('[4] lists: ' + str(lists))
+
+print('[5] User input')
+name = input('What is your name? > ')
+print('Hi ' + name)
+
+print('[6] User input(numerical input)')
+age = input ('How old are you? > ')
+print(' ' + age + ', type: ' + str(type(age)))
 
 # CH1 - END'''
 
@@ -71,16 +79,44 @@ print('[1] Tom\'s age is ' + str(tom['age'])  + ', gender is ' + tom['gender'])
 tom['glasses'] = "none"
 print('[2] Tom\'s glasses is ' + tom['glasses'])
 
-print('[3] display dictionary')
+print('[3] display key and value')
 for key, item in tom.items():
-  print(' ' + key + ' : ' + str(item))
+  print(' ' + key + ', ' + str(item))
 
+print('[4] display key')
+for key in tom.keys():
+  print(' ' + key)
+
+print('[5] display values')
+for value in tom.values():
+  print(' ' + str(value))
 
 # CH3 - END'''
+
 
 """ CH4 if 조건문과 while 반복문 (if statements and while loops) """
 #''' CH4 - START
 print('\n\nChapter 4')
+
+print('[1] Conditional Test')
+x = 10
+print(' == : ' + x == 10)
+print(' != : ' + x != 10)
+print(' >  : ' + x >  5)
+print(' >= : ' + x >= 10)
+print(' <  : ' + x < 5)
+print(' <= : ' + x <= 10)
+
+print('[2] Conditional Test with lists')
+basket = ['apple', 'banana', 'grape']
+print(' Q: Is there an apple in lists ? A: ' + 'apple' in basket)
+print(' Q: Is there an melon in lists ? A: ' + 'melon' in basket)
+
+print('[3] boolean values')
+enable = True
+
+
+
 # CH4 - END'''
 
 """ CH5 함수 (Functions) """
