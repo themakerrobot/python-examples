8433968cb963837e3bfa53d1aa5fa739aef0c1d7
leeyunjai82 <leeyunjai@circul.us>
2022-08-15 16:15:17 +0900

    update

--- cctv/day1/ex1_03_08.py
+++ cctv/day1/ex1_03_08.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from datetime import datetime
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -13,11 +12,11 @@ socketio = SocketManager(app=app)
 
 @app.get('/', response_class=HTMLResponse)
 async def main(request: Request):
-  return templates.TemplateResponse("ex1_03_07.html", {"request":request})
+  return templates.TemplateResponse("ex1_03_08.html", {"request":request})
 
-@app.sio.on('time')
+@app.sio.on('message')
 async def f_message(sid, msg=None):
-  await app.sio.emit('time', str(datetime.now()))
+  await app.sio.emit('message', "Your Message: " + msg)
 
 if __name__ == '__main__':
-  uvicorn.run("ex1_03_07:app", host="0.0.0.0", port=8080)
+  uvicorn.run("ex1_03_08:app", host="0.0.0.0", port=8080)
--- cctv/day1/ex1_03_09.py
+++ cctv/day1/ex1_03_09.py
@@ -5,6 +5,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from datetime import datetime
+import base64
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -13,11 +14,13 @@ socketio = SocketManager(app=app)
 
 @app.get('/', response_class=HTMLResponse)
 async def main(request: Request):
-  return templates.TemplateResponse("ex1_03_07.html", {"request":request})
+  return templates.TemplateResponse("ex1_03_09.html", {"request":request})
 
-@app.sio.on('time')
-async def f_message(sid, msg=None):
-  await app.sio.emit('time', str(datetime.now()))
+@app.sio.on('image')
+async def f_image(sid, msg=None):
+  with open('./test.jpg', 'rb') as img:
+    base64_string = base64.b64encode(img.read()).decode('utf-8')
+  await app.sio.emit("image", "data:image/jpeg;charset=utf-8;base64,"+base64_string)
 
 if __name__ == '__main__':
-  uvicorn.run("ex1_03_07:app", host="0.0.0.0", port=8080)
+  uvicorn.run("ex1_03_09:app", host="0.0.0.0", port=8080)
